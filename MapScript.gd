56a8edd53a4b81500f6d23a6bf5a051a7199d644
Kegue <henriquevinagre@tecnico.ulisboa.pt>
2022-01-29 21:45:11 +0000

    nao me lembro do que fiz

--- MapScript.gd
+++ MapScript.gd
@@ -29,16 +29,20 @@ func swap_entity_collisions():
 		$Dim1/Player.set_collision_mask_bit(2,true)
 		$Dim1/Tile1.modulate.a = 0
 		$Dim2/Tile2.modulate.a = 1
+		$ParallaxBackground/ParallaxLayer/Sprite.visible = false
 		$ParallaxBackground/ParallaxLayer2/Sprite.visible = false
 		$ParallaxBackground/ParallaxLayer3/Sprite.visible = true
+		$ParallaxBackground/ParallaxLayer4/Sprite.visible = true
 	else:
 		
 		$Dim1/Player.set_collision_mask_bit(1,true)
 		$Dim1/Player.set_collision_mask_bit(2,false)
 		$Dim1/Tile1.modulate.a = 1
 		$Dim2/Tile2.modulate.a = 0
+		$ParallaxBackground/ParallaxLayer/Sprite.visible = true
 		$ParallaxBackground/ParallaxLayer2/Sprite.visible = true
 		$ParallaxBackground/ParallaxLayer3/Sprite.visible = false
+		$ParallaxBackground/ParallaxLayer4/Sprite.visible = false
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
