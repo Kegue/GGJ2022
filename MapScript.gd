faafec5279790a8b9aef789ab4c2c53219d9232d
JoaoRaposo <joao.p.raposo@tecnico.ulisboa.pt>
2022-01-29 00:02:09 +0000

    SwapDimensions_29_00:02

--- MapScript.gd
+++ MapScript.gd
@@ -13,17 +13,27 @@ func  _process(delta):
 		$Dim1/Player.position = $Dim1/P1_spawn.position
 		print("Reseted player pos")
 	
-	print($Dim1/Player.position)
-	
 
 func swap_entity_collisions():
 	print("swapped dimension")
+	
 	if Global.alternate_dim:
+		
+		for vert in $Dim1/Player.get_node("CollisionPolygon2D").polygon:
+			if $Dim1/Tile1/Tilemap1.get_cellv($Dim1/Player.to_local(vert)) != TileMap.INVALID_CELL:
+				print("Colliding with the map! Not changing dimensions")
+				return
+		
 		$Dim1/Player.set_collision_layer_bit(1,false)
 		$Dim1/Player.set_collision_layer_bit(2,true)
 		$Dim1/Tile1.modulate.a = 0.5
 		$Dim2/Tile2.modulate.a = 1
 	else:
+		
+		for vert in $Dim1/Player.get_node("CollisionPolygon2D").polygon:
+			if $Dim2/Tile2/Tilemap2.get_cellv($Dim1/Player.to_local(vert)) != TileMap.INVALID_CELL:
+				print("Colliding with the map! Not changing dimensions")
+				return
 		$Dim1/Player.set_collision_layer_bit(1,true)
 		$Dim1/Player.set_collision_layer_bit(2,false)
 		$Dim1/Tile1.modulate.a = 1
