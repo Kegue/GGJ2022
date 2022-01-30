adf8728f30b548750310f55e1aba441e2428a357
Afsouto17 <andreferreirasouto123@gmail.com>
2022-01-30 14:22:26 +0000

    music on/off fixed

--- Options.gd
+++ Options.gd
@@ -4,11 +4,13 @@ extends Control
 func _ready():
 	if settings.music_enabled == false:
 		$Music/OnOf/CheckButton.pressed = false
+	else:
+		$Music/OnOf/CheckButton.pressed = true
 
 
 
 func _on_Music_toggled(button_pressed):
-	settings.music_enabled = !settings.music_enabled
+	settings.music_enabled = $Music/OnOf/CheckButton.pressed
 
 
 func _on_Back_pressed():
